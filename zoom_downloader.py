2f4a7e9d45087c1cc7fc6b49217e1ee4b5f467d2
FunnyPocketBook <dang.yingkai@gmail.com>
2020-04-07 18:04:51 +0200

    Beauty changes

--- zoom_downloader.py
+++ zoom_downloader.py
@@ -13,7 +13,7 @@ logging.basicConfig(filename=os.path.join(os.path.dirname(__file__), "zoom_downl
                     level=logging.INFO,
                     format="%(asctime)s %(message)s")
 
-
+# If no arguments have been given, get the URLs from conf.json
 URLS = sys.argv
 if (len(URLS) > 1):
     URLS.pop(0)
@@ -21,8 +21,8 @@ else:
     URLS = config.url
 
 
-def get_video_link(url):
-    session = requests.Session()
+def zoom_downloader(url):
+    session = requests.Session() # New session for each video to keep cookies
     req = session.get(url)
     if req.status_code != 200:
         print("Error %d for %s" % (req.status_code, url))
@@ -32,11 +32,11 @@ def get_video_link(url):
     topic = page.find("title").text
     video = page.find("video")
     video_request = get_video(session, video.contents[3].attrs["src"])
-    download_file(topic, video_request)
+    save_file(topic, video_request)
 
-
+# Loads whole video. This takes a while depending on your internet connection since it's actually a partial file.
 def get_video(session, url):
-    print("Loading video, this may take 20 seconds...")
+    print("Downloading video, this may take take a while...")
     custom_headers = {
         "Accept": "video/webm,video/ogg,video/*;q=0.9,application/ogg;q=0.7,audio/*;q=0.6,*/*;q=0.5",
         "Accept-Language": "en-US,en;q=0.5",
@@ -48,17 +48,17 @@ def get_video(session, url):
     return session.get(url, headers=custom_headers)
 
 
-def download_file(name, request):
-    name = name + ".mp4"
+def save_file(fileName, request):
+    name = fileName + ".mp4"
     f = open(name, "wb")
-    print("Downloading...")
+    print("Saving \"%s\" to computer..." % (name))
     for chunk in request.iter_content(chunk_size=255): 
         if chunk:
             f.write(chunk)
-    print("Done")
+    print("Done.")
     f.close()
 
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=config.workers) as exec:
     for url in URLS:
-        exec.submit(get_video_link, url)
+        exec.submit(zoom_downloader, url)
